3899261aea0700e80f9510d48962ca91f74f27db
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-03-13 15:48:54 -0500

    First migration

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
 // Update with your config settings.
 
+require('dotenv').config();
+
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      database: 'process.env.DB_DATABASE',
-      user: 'process.env.DB_USER',
-      password: 'process.env.DB_PASSWORD'
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     }
   },
 
